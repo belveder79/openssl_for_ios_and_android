c1303918f60d1377f9a6e5ff81a8f9847622d886
Clemens Arth <clemens@ar4.io>
2020-12-05 17:44:23 +0100

    added build-all script

---
+++ tools/runAll.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export ANDROID_NDK_ROOT=~/Development/Android/android-ndk-r19c
+export ANDROID_HOME=~/Development/Android/sdk
+export ANDROID_NDK_r16b=~/Development/Android/android-ndk-r16b
+export POLLY_ROOT=~/Development/AR4/VIZARIO.FIND/polly_bel
+
+/bin/bash build-android-openssl.sh;
+/bin/bash build-android-nghttp2.sh;
+/bin/bash build-android-curl.sh;
+/bin/bash build-ios-openssl.sh;
+/bin/bash build-ios-nghttp2.sh;
+/bin/bash build-ios-curl.sh;
+/bin/bash collapseFw-ios-openssl.sh;
+/bin/bash collapseFw-ios-nghttp2.sh;
+/bin/bash collapseFw-ios-curl.sh;
+/bin/bash prepareUnity.sh;
