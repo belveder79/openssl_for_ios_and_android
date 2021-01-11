f7124fa766aa68a205ed40fc7f736f47800a31e6
Clemens Arth <clemens@ar4.io>
2021-01-11 20:11:34 +0100

    removed curltool

--- tools/copyToUnity.sh
+++ tools/copyToUnity.sh
@@ -12,7 +12,7 @@ for ((i = 0; i < ${#ABIS[@]}; i++)); do
   echo "running on ${ABIS[i]} ..."
   APIOUT=$OUTDIR/Android/libs/${ABIS[i]}
   cp ../output/android/curl-${ABIS[i]}/lib/libcurl.so $APIOUT/
-  cp ../output/android/curl-${ABIS[i]}/lib/libcurltool.so $APIOUT/
+  #cp ../output/android/curl-${ABIS[i]}/lib/libcurltool.so $APIOUT/
   cp ../output/android/nghttp2-${ABIS[i]}/lib/libnghttp2.so $APIOUT/
   cp ../output/android/openssl-${ABIS[i]}/lib/libcurlcrypto.so $APIOUT/
   cp ../output/android/openssl-${ABIS[i]}/lib/libcurlssl.so $APIOUT/
@@ -21,7 +21,7 @@ done
 echo "Copying iOS... "
 
 cp -R ../output/ios/curl-framework/curl.framework $OUTDIR/iOS/
-cp -R ../output/ios/curl-framework/curltool.framework $OUTDIR/iOS/
+#cp -R ../output/ios/curl-framework/curltool.framework $OUTDIR/iOS/
 cp -R ../output/ios/nghttp2-framework/nghttp2.framework $OUTDIR/iOS/
 cp -R ../output/ios/openssl-framework/libcrypto.framework $OUTDIR/iOS/
 cp -R ../output/ios/openssl-framework/openssl.framework $OUTDIR/iOS/
